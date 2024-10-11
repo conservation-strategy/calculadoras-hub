d47c7af8aa6f10884218c869c2dc25c7a5dcb9ac
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-11 08:57:50 -0300

    fonts

---
+++ app/fonts.js
@@ -0,0 +1,6 @@
+import { Gudea, Duru_Sans, Doppio_One, } from 'next/font/google';
+
+export const gudea = Gudea({ subsets: ['latin'] });
+export const duru_sans = Duru_Sans({ subsets: ['latin'] });
+export const doppio_one = Doppio_One({ subsets: ['latin'] });
+
