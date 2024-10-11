e530629556a724917c6e3dc8b4368d18df8c7e71
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-11 09:26:53 -0300

    fix: font weight

--- app/fonts.js
+++ app/fonts.js
@@ -1,6 +1,6 @@
 import { Gudea, Duru_Sans, Doppio_One, } from 'next/font/google';
 
-export const gudea = Gudea({ subsets: ['latin'] });
-export const duru_sans = Duru_Sans({ subsets: ['latin'] });
-export const doppio_one = Doppio_One({ subsets: ['latin'] });
+export const gudea = Gudea({ subsets: ['latin'], weight: ['400', '700'] });
+export const duru_sans = Duru_Sans({ subsets: ['latin'], weight: '400' });
+export const doppio_one = Doppio_One({ subsets: ['latin'] , weight: '400' });
 
