ece9f3cb1c7eaafcab770e221718b98ef4bbee38
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-11 18:07:05 -0300

    font: droid-sans

--- app/fonts.js
+++ app/fonts.js
@@ -1,6 +1,29 @@
 import { Gudea, Duru_Sans, Doppio_One, } from 'next/font/google';
+import localFont from 'next/font/local'
 
-export const gudea = Gudea({ subsets: ['latin'], weight: ['400', '700'] });
-export const duru_sans = Duru_Sans({ subsets: ['latin'], weight: '400' });
-export const doppio_one = Doppio_One({ subsets: ['latin'] , weight: '400' });
+export const droid_sans = localFont({ 
+  src: '../public/fonts/DroidSans.ttf',
+  display: 'swap',
+});
+
+export const droid_sans_bold = localFont({ 
+    src: '../public/fonts/DroidSans-Bold.ttf',
+    display: 'swap',
+  });
+
+export const gudea = Gudea({ 
+    subsets: ['latin'], 
+    weight: ['400', '700'] ,
+    display: 'swap'
+});
+export const duru_sans = Duru_Sans({
+    subsets: ['latin'], 
+    weight: '400',
+    display: 'swap'
+});
+export const doppio_one = Doppio_One({ 
+    subsets: ['latin'] , 
+    weight: '400' ,
+    display: 'swap'
+});
 
