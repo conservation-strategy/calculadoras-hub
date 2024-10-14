a375f004167230f74ce0790d050cda72e1ed0ef1
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-14 14:18:51 -0300

    calculator-button

---
+++ app/components/calculators/calculator-button.jsx
@@ -0,0 +1,52 @@
+import styles from './calculators.module.css';
+import { doppio_one } from '@/app/fonts';
+import Image from 'next/image';
+
+export function CalculatorButton({ 
+    className, 
+    circleBackground, 
+    icon, 
+    title, 
+    description
+}) {
+    
+    return (
+        <a className={`${styles.calculatorButton} ${className}`}
+        href=''
+        target='_blank'
+        rel='noopener noreferrer'
+        >
+            <div 
+            className={styles.circle}
+            style={{ backgroundColor: circleBackground }}
+            >
+                {icon}
+            </div>
+            <div className={styles.textContainer}>
+                <div className={`${styles.title} ${doppio_one.className}`}>
+                    {title}
+                </div>
+                <div className={styles.description}>
+                    {description}
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export function Icon ({ className, icon }) {
+    return (
+        <div className={className}>
+            <Image
+            src={icon}
+            alt={`${icon.slice(8)}`}
+            height={0}
+            width={0}
+            style={{
+                width: '100%',
+                height: 'auto',                            
+            }}
+            />
+        </div>
+    )
+}
