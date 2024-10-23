82979d12b6b813d5a5dc88b76f928f43090b9410
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-23 07:46:12 -0300

    fix: metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { gudea } from "./fonts";
 import { AppProvider } from "./context/provider";
 
-const baseUrl = "https://calculators.conservation-strategy.org/"
+const baseUrl = "https://calculators.conservation-strategy.org"
 
 export const metadata = {
   title: "CSF Calculators Hub | Tools to support decision-making",
@@ -11,7 +11,7 @@ export const metadata = {
     title: "CSF Calculators Hub | Tools to support decision-making",
     description: "Access CSF's free tools to calculate environmental costs and benefits, speeding up planning, fundraising, and fine calculations for better decision-making.",
     images: [{
-      url: `${baseUrl}/assets/hub-link-image.png`,
+      url: `${baseUrl}/assets/hub-imagem-link.png`,
       width: 1200,
       height: 630,
     }],
@@ -20,7 +20,7 @@ export const metadata = {
     card: 'summary_large_image',
     title: "CSF Calculators Hub | Tools to support decision-making",
     description: "Access CSF's free tools to calculate environmental costs and benefits, speeding up planning, fundraising, and fine calculations for better decision-making.",
-    images: [`${baseUrl}/assets/hub-link-image.png`],
+    images: [`${baseUrl}/assets/hub-imagem-link.png`],
   },
 };
 
