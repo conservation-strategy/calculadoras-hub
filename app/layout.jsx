5369fb845b46179a34c9531a1d9ea4e3aa237abf
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-23 07:26:28 -0300

    metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,9 +2,26 @@ import "./globals.css";
 import { gudea } from "./fonts";
 import { AppProvider } from "./context/provider";
 
+const baseUrl = "https://calculators.conservation-strategy.org/"
+
 export const metadata = {
-  title: "Calculators Hub",
-  description: "A project by the Conservation Strategy Fund",
+  title: "CSF Calculators Hub | Tools to support decision-making",
+  description: "Access CSF's free tools to calculate environmental costs and benefits, speeding up planning, fundraising, and fine calculations for better decision-making.",
+  openGraph: {
+    title: "CSF Calculators Hub | Tools to support decision-making",
+    description: "Access CSF's free tools to calculate environmental costs and benefits, speeding up planning, fundraising, and fine calculations for better decision-making.",
+    images: [{
+      url: `${baseUrl}/assets/hub-link-image.png`,
+      width: 1200,
+      height: 630,
+    }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "CSF Calculators Hub | Tools to support decision-making",
+    description: "Access CSF's free tools to calculate environmental costs and benefits, speeding up planning, fundraising, and fine calculations for better decision-making.",
+    images: [`${baseUrl}/assets/hub-link-image.png`],
+  },
 };
 
 export default function RootLayout({ children }) {
