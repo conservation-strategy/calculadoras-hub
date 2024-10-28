b6a8c5a2616aaf2f57fc7a38a5d47956cad8db23
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-28 14:26:27 -0300

    responsive: midsection heading

--- app/components/midsection/midsection.jsx
+++ app/components/midsection/midsection.jsx
@@ -2,7 +2,7 @@
 import { droid_sans_bold } from '@/app/fonts';
 import styles from './midsection.module.css';
 import Image from 'next/image';
-import { useLanguage, ENGLISH } from '@/app/context/provider';
+import { useLanguage, ENGLISH, SPANISH, PORTUGUESE } from '@/app/context/provider';
 
 export default function MidSection () {
     const { content, language } = useLanguage();
@@ -10,12 +10,33 @@ export default function MidSection () {
 
     return (
         <div className={styles.section}>
-            <div className={styles.headingContainer}>
+            <div className={`${styles.headingContainer} ${language !== ENGLISH ? styles.translation : ''}`}>
                 <div className={`${styles.title} ${droid_sans_bold.className}`}>
                     {midsection.title}
                 </div>
                 <div className={`${styles.heading} ${droid_sans_bold.className} ${language !== ENGLISH ? styles.translation : ''}`}>
-                    {midsection.heading}
+                    {language === SPANISH 
+                        ? <span className={styles.translation}>
+                            <span className={styles.letterSpaceES_1}>Herramientas</span>
+                            {' '}
+                            <span className={styles.letterSpaceES_2}>para la</span>                            
+                            {' '}
+                            <span className={styles.letterSpaceES_3}>toma</span>
+                            {' '}
+                            <span className={styles.letterSpaceES_4}>de decisiones</span>
+                        </span>
+                        : language === PORTUGUESE 
+                            ? <span className={`${styles.translation} ${styles.fontSizePT_1}`}>
+                                <span className={`${styles.letterSpacePT_1} `}>Ferramentas</span>
+                                {' '}
+                                <span className={styles.letterSpacePT_4}>para</span>
+                                {' '}
+                                <span className={styles.letterSpacePT_2}>tomada</span>
+                                {' '}
+                                <span className={styles.letterSpacePT_3}>de decisão</span>
+                                </span>
+                            : <span>Tools to support decision making</span>
+                    }
                 </div>
                 <div className={styles.subHeading}>
                     {midsection.subHeading}
