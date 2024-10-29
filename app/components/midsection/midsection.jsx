a2410c5bdb7d018b0268c4b18a9c390fd32c6c6e
Pedro Franceschin <pbfranceschin@gmail.com>
2024-10-29 09:28:38 -0300

    responsive: midsection

--- app/components/midsection/midsection.jsx
+++ app/components/midsection/midsection.jsx
@@ -38,7 +38,7 @@ export default function MidSection () {
                             : <span>Tools to support decision-making</span>
                     }
                 </div>
-                <div className={`${styles.subHeading} ${language !== ENGLISH ? styles.marginLeft : '' }`}>
+                <div className={`${styles.subHeading} ${language !== ENGLISH ? styles.marginLeft : styles.english }`}>
                     {midsection.subHeading}
                 </div>
             </div>
