7e4e713990a84fb9150ea3acf1d231340d6fb629
Pedro Franceschin <pbfranceschin@gmail.com>
2024-11-01 07:55:36 -0300

    fix: midsection class names

--- app/components/midsection/midsection.jsx
+++ app/components/midsection/midsection.jsx
@@ -39,7 +39,7 @@ export default function MidSection () {
                     }
                 </div>
                 <div className={
-                    `${styles.subHeading} ${language !== ENGLISH ? styles.marginLeft : styles.english } ${language !== ENGLISH ? styles.translation : ''}`
+                    `${styles.subHeading} ${language !== ENGLISH ? styles.marginLeft : styles.english } ${language !== ENGLISH ? styles.notEnglish : ''}`
                     }>
                     {midsection.subHeading}
                 </div>
